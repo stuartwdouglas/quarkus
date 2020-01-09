220e99d162aea7467fe1b23856450c0651274bd8
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-01-10 02:27:09 +1100

    More fixes

--- test-framework/arquillian/src/main/java/io/quarkus/arquillian/RequestContextLifecycle.java
+++ test-framework/arquillian/src/main/java/io/quarkus/arquillian/RequestContextLifecycle.java
@@ -6,7 +6,6 @@ import org.jboss.arquillian.test.spi.event.suite.Before;
 import org.jboss.logging.Logger;
 
 import io.quarkus.arc.Arc;
-import io.quarkus.arc.ArcContainer;
 
 /**
  * Activates request context before test runs and shuts it down afterwards
@@ -18,18 +17,30 @@ public class RequestContextLifecycle {
     private static final int DEFAULT_PRECEDENCE = 100;
 
     public void on(@Observes(precedence = DEFAULT_PRECEDENCE) Before event) throws Throwable {
-        ArcContainer container = Arc.container();
-        if (container != null && container.isRunning()) {
-            container.requestContext().activate();
-            LOGGER.debug("RequestContextLifecycle activating CDI Request context.");
+        //we are outside the runtime class loader, so we don't have direct access to the container
+        Class<?> arcClz = Thread.currentThread().getContextClassLoader().loadClass(Arc.class.getName());
+        Object container = arcClz.getMethod("container").invoke(null);
+        if (container != null) {
+            boolean running = (boolean) container.getClass().getMethod("isRunning").invoke(container);
+            if (running) {
+                Object context = container.getClass().getMethod("requestContext").invoke(container);
+                context.getClass().getMethod("activate").invoke(context);
+                LOGGER.debug("RequestContextLifecycle activating CDI Request context.");
+            }
         }
     }
 
     public void on(@Observes(precedence = DEFAULT_PRECEDENCE) After event) throws Throwable {
-        ArcContainer container = Arc.container();
-        if (container != null && container.isRunning()) {
-            container.requestContext().terminate();
-            LOGGER.debug("RequestContextLifecycle shutting down CDI Request context.");
+        //we are outside the runtime class loader, so we don't have direct access to the container
+        Class<?> arcClz = Thread.currentThread().getContextClassLoader().loadClass(Arc.class.getName());
+        Object container = arcClz.getMethod("container").invoke(null);
+        if (container != null) {
+            boolean running = (boolean) container.getClass().getMethod("isRunning").invoke(container);
+            if (running) {
+                Object context = container.getClass().getMethod("requestContext").invoke(container);
+                context.getClass().getMethod("terminate").invoke(context);
+                LOGGER.debug("RequestContextLifecycle activating CDI Request context.");
+            }
         }
     }
 }
