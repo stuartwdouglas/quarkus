aa58f58ed37a1e56aa98bc0a5785429a2cf0119b
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-08-26 15:01:09 +1000

    Add support for config via //CONFIG

--- core/runtime/src/main/java/io/quarkus/runtime/jbang/JBangIntegration.java
+++ core/runtime/src/main/java/io/quarkus/runtime/jbang/JBangIntegration.java
@@ -8,7 +8,22 @@ import io.quarkus.launcher.JBangRuntimeBuilder;
 
 public class JBangIntegration {
 
-    public static Map<String, byte[]> postBuild(Path classesDir, Path pomFile, List<Map.Entry<String, Path>> dependencies) {
+    public static final String CONFIG = "//CONFIG";
+
+    public static Map<String, byte[]> postBuild(Path classesDir, Path pomFile, List<Map.Entry<String, Path>> dependencies,
+            List<String> comments) {
+        for (String comment : comments) {
+            //we allow config to be provided via //CONFIG name=value
+            if (comment.startsWith(CONFIG)) {
+                String conf = comment.substring(CONFIG.length()).trim();
+                int equals = conf.indexOf("=");
+                if (equals == -1) {
+                    throw new RuntimeException("invalid config  " + comment);
+                }
+                System.setProperty(conf.substring(0, equals), conf.substring(equals + 1));
+            }
+        }
+
         return JBangRuntimeBuilder.postBuild(classesDir, pomFile, dependencies);
     }
 
