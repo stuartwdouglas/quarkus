aa0a6bb724aa0716851d8c9c3628a93153889283
Erin Schnabel <ebullient@redhat.com>
2021-06-11 15:53:52 -0400

    resteasy-classic: stash resolved template @Path
    Co-authored-by: Ken Finnigan <ken@kenfinnigan.me>

---
+++ extensions/resteasy-classic/resteasy/runtime/src/main/java/io/quarkus/resteasy/runtime/QuarkusRestPathTemplateInterceptor.java
@@ -0,0 +1,49 @@
+package io.quarkus.resteasy.runtime;
+
+import java.lang.annotation.Annotation;
+import java.util.Set;
+
+import javax.annotation.Priority;
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+
+import io.quarkus.arc.ArcInvocationContext;
+import io.quarkus.vertx.http.runtime.CurrentVertxRequest;
+import io.vertx.core.http.impl.HttpServerRequestInternal;
+
+@SuppressWarnings("unused")
+@QuarkusRestPathTemplate
+@Interceptor
+@Priority(Interceptor.Priority.LIBRARY_BEFORE + 10)
+public class QuarkusRestPathTemplateInterceptor {
+    @Inject
+    CurrentVertxRequest request;
+
+    @AroundInvoke
+    Object restMethodInvoke(InvocationContext context) throws Exception {
+        QuarkusRestPathTemplate annotation = getAnnotation(context);
+        try {
+            return context.proceed();
+        } finally {
+            if (annotation != null) {
+                ((HttpServerRequestInternal) request.getCurrent().request()).context().putLocal("UrlPathTemplate",
+                        annotation.value());
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    static QuarkusRestPathTemplate getAnnotation(InvocationContext context) {
+        Set<Annotation> annotations = (Set<Annotation>) context.getContextData()
+                .get(ArcInvocationContext.KEY_INTERCEPTOR_BINDINGS);
+
+        for (Annotation a : annotations) {
+            if (a instanceof QuarkusRestPathTemplate) {
+                return (QuarkusRestPathTemplate) a;
+            }
+        }
+        return null;
+    }
+}
